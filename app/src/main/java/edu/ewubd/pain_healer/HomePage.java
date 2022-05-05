f9d5833d467dc2338ca7fcd0695cb8725208d06b
Roy-000 <kawshik400@gmail.com>
2022-05-06 02:36:05 +0600

    kawshik old work

--- app/src/main/java/edu/ewubd/pain_healer/HomePage.java
+++ app/src/main/java/edu/ewubd/pain_healer/HomePage.java
@@ -2,9 +2,12 @@ package edu.ewubd.pain_healer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,72 +24,144 @@ import com.google.firebase.database.ValueEventListener;
 
 public class HomePage extends AppCompatActivity {
 
-    private TextView ivPic1, ivPic2, ivPic3;
-    private Button btnLogout;
-    private String role,userId;
-    private DatabaseReference mDatabase;
-    private FirebaseAuth mAuth;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        ivPic1 = findViewById(R.id.post);
-        ivPic2 = findViewById(R.id.profile);
-        ivPic3 = findViewById(R.id.admin);
-        btnLogout = findViewById(R.id.btnLogout);
 
-        ivPic1.setOnClickListener(view -> {
-            Intent i = new Intent(HomePage.this, PostActivity.class);
-            startActivity(i);
-        });
+        //toolbar
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        ivPic2.setOnClickListener(view -> {
-            Intent i = new Intent(HomePage.this, ProfileActivity.class);
-            startActivity(i);
-        });
-
-        ivPic3.setOnClickListener(view -> {
-            Intent i2 = new Intent(HomePage.this, PostActivity.class);
-            startActivity(i2);
-        });
-        mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        userId = mAuth.getCurrentUser().getUid();
-
-        mDatabase.child("Users").child(userId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                role = snapshot.child("role").getValue().toString();
-                if(role.equals("admin")){
-                    ivPic3.setVisibility(View.VISIBLE);
-                    ivPic1.setVisibility(View.GONE);
-                }
-                else {
-                    ivPic3.setVisibility(View.GONE);
-                    ivPic1.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_24);
 
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-                FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(HomePage.this, SignInPage.class);
-                startActivity(i);
+    }
 
-            }
-        });
+    // button to toolbar
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+
+
+        if(id==R.id.action_notification){
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  <--_________________________ Humur code start____________________________________________________________-->
+
+//public class HomePage extends AppCompatActivity {
+//
+//    private TextView ivPic1, ivPic2, ivPic3;
+//    private Button btnLogout;
+//    private String role,userId;
+//    private DatabaseReference mDatabase;
+//    private FirebaseAuth mAuth;
+//
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_home_page);
+//
+//        ivPic1 = findViewById(R.id.post);
+//        ivPic2 = findViewById(R.id.profile);
+//        ivPic3 = findViewById(R.id.admin);
+//        btnLogout = findViewById(R.id.btnLogout);
+//
+//        ivPic1.setOnClickListener(view -> {
+//            Intent i = new Intent(HomePage.this, PostActivity.class);
+//            startActivity(i);
+//        });
+//
+//        ivPic2.setOnClickListener(view -> {
+//            Intent i = new Intent(HomePage.this, ProfileActivity.class);
+//            startActivity(i);
+//        });
+//
+//        ivPic3.setOnClickListener(view -> {
+//            Intent i2 = new Intent(HomePage.this, PostActivity.class);
+//            startActivity(i2);
+//        });
+//        mAuth = FirebaseAuth.getInstance();
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+//        userId = mAuth.getCurrentUser().getUid();
+//
+//        mDatabase.child("Users").child(userId).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                role = snapshot.child("role").getValue().toString();
+//                if(role.equals("admin")){
+//                    ivPic3.setVisibility(View.VISIBLE);
+//                    ivPic1.setVisibility(View.GONE);
+//                }
+//                else {
+//                    ivPic3.setVisibility(View.GONE);
+//                    ivPic1.setVisibility(View.VISIBLE);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//                FirebaseAuth.getInstance().signOut();
+//                Intent i = new Intent(HomePage.this, SignInPage.class);
+//                startActivity(i);
+//
+//            }
+//        });
+//    }
+//}
+
+//<!--_________________________ Humur code end____________________________________________________________-->
